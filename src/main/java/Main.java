d263f6a15142cf5634cc28d607f31aefcd27e68e
gutierrez-anthony <gutierrez.anthony@student.greenriver.edu>
2024-01-05 10:42:13 -0800

    Added a printMessage method and print hello, world to console.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("hello, world");
+    }
 
+    public void printMessage(String message) {
+        System.out.println(message);
     }
 }
